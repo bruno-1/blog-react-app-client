4a2d7aee6373e13e00d1919cf7090e251ba53c38
Bruno <bruno_benaglia2@outlook.com>
2022-01-27 18:56:51 -0300

    React sidebar example

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -3,7 +3,31 @@ import "./sidebar.css";
 function Sidebar() {
   return (
     <div className="sidebar">
-      sidebar
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img src="https://images.unsplash.com/photo-1643186921363-d9fb2191f798?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At similique error a quibusdam aut? Voluptatibus saepe blanditiis maxime!</p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+        <i className="sidebarIcon fab fa-facebook-square"></i>
+        <i className="sidebarIcon fab fa-twitter-square"></i>
+        <i className="sidebarIcon fab fa-pinterest-square"></i>
+        <i className="sidebarIcon fab fa-instagram-square"></i>
+        </div>
+      </div>
     </div>
   );
 }
